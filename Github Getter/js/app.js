41582007f203c159547d4513806c2f073a015126
gogojojo <joseph.gochua@gmail.com>
2015-05-03 04:10:12 -0700

    putting things into functions so that the code looks better

--- Github Getter/js/app.js
+++ Github Getter/js/app.js
@@ -1,3 +1,5 @@
+// note: not using legacy seach because github api suggests using un depreciated version
+
 var searchHistory = {};
 
 $(function(){
@@ -7,49 +9,22 @@ $(function(){
     searchHistory = localStorage.getItem('localCache');
     searchHistory = JSON.parse(searchHistory);
   }
-
   $('#searchsub').click(function(){
     var searchTerm = $('#search').val();
     //if the search term has already been searched for then no need for ajax call
     if (Object.keys(searchHistory).indexOf(searchTerm) != -1) {
-      var searchResults = searchHistory[searchTerm].items;
-      // clear out all list items in ul
-      $('#results').empty();
-          // if the search term doesnt return any results
-      if (searchResults.length === 0) {
-        $('#results').append("<li> does not match any repositories </li>");
-      } else {
-        for (var i = 0; i < searchResults.length; i ++) {
-          $('#results').append(
-            "<li class='result'> <h1> Name:"+searchResults[i].name+" Owner:"+searchResults[i].owner.login+"</h1>"
-            +"<h2>"+searchResults[i].description+" "+searchResults[i].url+"</h2> </li>"
-          );
-        }
-      }
-
+      // running the function that shows search results
+      createlist(searchTerm);
     } else {
       $.ajax({
-        // note: not using legacy seach because github api suggests using un depreciated version
         // Returning the top 25 repositories based on stars
         url: "https://api.github.com/search/repositories?q="+searchTerm+"&sort=stars&per_page=25",
         success: function(result){
           searchHistory[searchTerm] = result;
           // storing search history object into local storage
           localStorage.setItem('localCache', JSON.stringify(searchHistory));
-          var searchResults = searchHistory[searchTerm].items;
-          // clear out all list items in ul
-          $('#results').empty();
-          // if the search term doesnt return any results
-          if (searchResults.length === 0) {
-            $('#results').append("<li> does not match any repositories </li>");
-          } else {
-            for (var i = 0; i < searchResults.length; i ++) {
-              $('#results').append(
-                "<li class='result'> <h1> Name:"+searchResults[i].name+" Owner:"+searchResults[i].owner.login+"</h1>"
-                +"<h2>"+searchResults[i].description+" "+searchResults[i].url+"</h2> </li>"
-              );
-            }
-          }
+          // running the function that shows search results
+          createlist(searchTerm);
         },
         error: function(result){
           $("#div1").html('does not exist');
@@ -62,5 +37,22 @@ $(function(){
   $(document).on('click', 'li', function(){ 
     $(this).children('h2').css('display','block');
   }); 
+
+  // function for creating the list of search returns
+  function createlist(searchTerm) {
+    var searchResults = searchHistory[searchTerm].items;
+    $('#results').empty();
+    // if the search term doesnt return any results
+    if (searchResults.length === 0) {
+        $('#results').append("<li> does not match any repositories </li>");
+    } else {
+      for (var i = 0; i < searchResults.length; i ++) {
+        $('#results').append(
+          "<li class='result'> <h1> Name:"+searchResults[i].name+" Owner:"+searchResults[i].owner.login+"</h1>"
+          +"<h2>"+searchResults[i].description+" "+searchResults[i].url+"</h2> </li>"
+        );
+      }
+    }
+  };
 
 });
