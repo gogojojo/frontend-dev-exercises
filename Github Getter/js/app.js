9c6a1ba1104ecbe769e40ec0959c5006583c57db
gogojojo <joseph.gochua@gmail.com>
2015-05-03 00:28:58 -0700

    finished caching search terms and creating lists for all returned results

--- Github Getter/js/app.js
+++ Github Getter/js/app.js
@@ -35,22 +35,37 @@
       }
     }
 */
+var searchHistory = {}
+var blah
+
 $(function(){
-  var list = ['one','two','three','four']
   $('#searchsub').click(function(){
-    var blah = $('#search').val();
-    console.log(blah)
-     $.ajax({
-      url: "https://api.github.com/repos/gogojojo/"+blah, 
-      success: function(result){
-        for (var i = 0; i < list.length; i ++) {
-          $("#results").append("<li>"+list[i]+"</li>");
+    searchTerm = $('#search').val();
+    // if the search term has already been searched for
+    if (Object.keys(searchHistory).indexOf(searchTerm) != -1) {
+      console.log('searched already')
+
+    } else {
+      $.ajax({
+        url: "https://api.github.com/legacy/repos/search/"+searchTerm, 
+        success: function(result){
+          searchHistory[searchTerm] = result
+          var searchResults = searchHistory[searchTerm].repositories
+          // clear out all list items in ul
+          $('#results').empty();
+          // if the search term doesnt return any results
+          if (searchResults.length === 0) {
+            $('#results').append("<li> does not match any repositories </li>")
+          } else {
+            for (var i = 0; i < searchResults.length; i ++) {
+              $('#results').append("<li>"+searchResults[i].name+" "+searchResults[i].owner+"</li>")
+            }
+          }
+        },
+        error: function(result){
+          $("#div1").html('does not exist')
         }
-        console.log(result.owner.login)
-      },
-      error: function(result){
-        $("#div1").html('does not exist')
-      }
-    });
+      });
+   }
   })
-})
+});
